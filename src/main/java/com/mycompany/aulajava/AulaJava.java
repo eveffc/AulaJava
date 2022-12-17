2ce1c5d38e9ef3b28a31d39cb70f3c8eeff2ed4e
Evelyn Santos <117581658+eveffc@users.noreply.github.com>
2022-12-17 19:29:11 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/aulajava/AulaJava.java
@@ -0,0 +1,11 @@
+package com.mycompany.aulajava;
+/**
+ *
+ * @author EvelynSantos
+ */
+public class AulaJava {
+
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
