6aa2865122970dc4c16a1f245b0ae2d5cc110994
Evelyn Santos <117581658+eveffc@users.noreply.github.com>
2022-12-17 20:19:41 -0300

    DataHora do sistema
    Exercício para pegar a data e a hora do sistema

---
+++ HoraDoSistema/src/main/java/com/mycompany/horadosistema/HoraDoSistema.java
@@ -0,0 +1,16 @@
+package com.mycompany.horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author EvelynSantos
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date datahora = new Date();
+        System.out.println("A data-hora é:");
+        System.out.println(datahora.toString());
+    }
+}
